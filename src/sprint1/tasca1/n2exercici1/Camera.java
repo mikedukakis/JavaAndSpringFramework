2caf74f21c63ab0466420eedde2c003887aa7171
Ignasi Moya <imoyaf+git@gmail.com>
2024-05-07 10:13:03 +0200

    Initial commit

---
+++ src/sprint1/tasca1/n2exercici1/Camera.java
@@ -0,0 +1,7 @@
+package sprint1.tasca1.n2exercici1;
+
+public interface Camera {
+
+    public void fotografiar();
+
+}
---
+++ src/sprint1/tasca1/n2exercici1/Rellotge.java
@@ -0,0 +1,7 @@
+package sprint1.tasca1.n2exercici1;
+
+public interface Rellotge {
+
+    public void alarma();
+
+}
