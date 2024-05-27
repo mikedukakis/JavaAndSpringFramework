6ca70077d454a8d9da50dacbe7737583c26ffe49
Ignasi <imoyaf@gmail.com>
2024-05-27 11:37:44 +0200

    Update Main.java

--- src/sprint1/tasca5/n1exercici4/Main.java
+++ src/sprint1/tasca5/n1exercici4/Main.java
@@ -1,48 +1,15 @@
 package sprint1.tasca5.n1exercici4;
 
 import java.io.*;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Directory dir = new Directory("..");
         File[] fileArray = dir.listContentsOfDir(dir.getDirPathName());
 
-        loopThroughContent(fileArray);
+        FileHandler.loopThroughContent(fileArray);
 
         File txtFile = new File(System.getProperty("user.dir").concat(File.separator).concat("directoryList.txt"));
-        readTxt(txtFile);
-    }
-
-    public static void loopThroughContent(File[] fileArray) {
-        for(File element : fileArray){
-            if(element.isDirectory()){
-                writeToFile(element + "(D)");
-                File[] subArray = element.listFiles();
-                loopThroughContent(subArray);
-            } else {
-                writeToFile(element + "(F)");
-            }
-        }
-    }
-
-    public static void writeToFile(String str) {
-        try (FileWriter fileWriter = new FileWriter("directoryList.txt", true);
-             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
-            bufferedWriter.write(str + "\n");
-        } catch (IOException e) {
-            System.out.println("An error occurred while trying to write to file");
-            e.printStackTrace();
-        }
-    }
-
-    public static void readTxt(File file) {
-        try(Scanner fileInput = new Scanner(file)) {
-            while(fileInput.hasNextLine()){
-                System.out.println(fileInput.nextLine());
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
+        FileHandler.readTxt(txtFile);
     }
 }
