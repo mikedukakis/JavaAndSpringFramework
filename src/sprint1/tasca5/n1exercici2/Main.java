2498d841ad7fb249cec4303d4554bc47d78ecb71
Ignasi <imoyaf@gmail.com>
2024-05-27 11:34:55 +0200

    Update Main.java

--- src/sprint1/tasca5/n1exercici2/Main.java
+++ src/sprint1/tasca5/n1exercici2/Main.java
@@ -3,21 +3,11 @@ package sprint1.tasca5.n1exercici2;
 import java.io.File;
 
 public class Main {
+
     public static void main(String[] args) {
-        Directory dir = new Directory("..");
+        Directory dir = new Directory(".");
         File[] fileArray = dir.listContentsOfDir(dir.getDirPathName());
-        loopThroughContent(fileArray);
+        FileHandler.loopThroughContent(fileArray);
     }
 
-    public static void loopThroughContent(File[] fileArray) {
-        for(File element : fileArray){
-            if(element.isDirectory()){
-                System.out.println(element + "(D)");
-                File[] subArray = element.listFiles();
-                loopThroughContent(subArray);
-            } else {
-                System.out.println(element + "(F)");
-            }
-        }
-    }
 }
