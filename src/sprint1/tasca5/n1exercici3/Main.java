0ca6ae0fbd4d7076bca813f5d7c1883a9602de17
Ignasi <imoyaf@gmail.com>
2024-05-27 11:36:38 +0200

    Update Main.java

--- src/sprint1/tasca5/n1exercici3/Main.java
+++ src/sprint1/tasca5/n1exercici3/Main.java
@@ -1,36 +1,11 @@
 package sprint1.tasca5.n1exercici3;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         Directory dir = new Directory("..");
         File[] fileArray = dir.listContentsOfDir(dir.getDirPathName());
-        loopThroughContent(fileArray);
-    }
-
-    public static void loopThroughContent(File[] fileArray) {
-        for(File element : fileArray){
-            if(element.isDirectory()){
-                writeToFile(element + "(D)");
-                File[] subArray = element.listFiles();
-                loopThroughContent(subArray);
-            } else {
-                writeToFile(element + "(F)");
-            }
-        }
-    }
-
-    public static void writeToFile(String str) {
-        try (FileWriter fileWriter = new FileWriter("directoryList.txt", true);
-             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
-            bufferedWriter.write(str + "\n");
-        } catch (IOException e) {
-            System.out.println("An error occurred while trying to write to file");
-            e.printStackTrace();
-        }
+        FileHandler.loopThroughContent(fileArray);
     }
 }
