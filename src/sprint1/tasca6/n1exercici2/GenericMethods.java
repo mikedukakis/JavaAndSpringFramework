c7e4ddb1fbc917c248d4efdd42c651c3bf301f11
Ignasi Moya <imoyaf+git@gmail.com>
2024-05-28 11:08:14 +0200

    First commit

---
+++ src/sprint1/tasca6/n1exercici2/GenericMethods.java
@@ -0,0 +1,11 @@
+package sprint1.tasca6.n1exercici2;
+
+public class GenericMethods {
+
+    public static <T, S, U> void printGenerics(T obj1, S obj2, U obj3) {
+        System.out.println(obj1);
+        System.out.println(obj2);
+        System.out.println(obj3);
+    }
+
+}
---
+++ src/sprint1/tasca6/n1exercici2/Main.java
@@ -0,0 +1,15 @@
+package sprint1.tasca6.n1exercici2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Person pers1 = new Person("Jane", "Doe", (byte)10);
+        String str = "Generics!";
+        char character = 'G';
+
+        GenericMethods.printGenerics(pers1, str, character);
+
+    }
+
+}
