378be8f24f2c7a2957b53a7150d85084debe3110
Ignasi <imoyaf@gmail.com>
2024-05-28 10:09:16 +0200

    Manually Update FileHandler.java

--- src/sprint1/tasca5/n1exercici4/FileHandler.java
+++ src/sprint1/tasca5/n1exercici4/FileHandler.java
@@ -1,24 +1,48 @@
 package sprint1.tasca5.n1exercici4;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
 public class FileHandler {
 
-    public static void loopThroughContent(File[] fileArray) {
-        for(File element : fileArray){
-            if(element.isDirectory()){
-                writeToFile(element + "(D)");
-                File[] subArray = element.listFiles();
-                loopThroughContent(subArray);
+    static ArrayList<String> fileList = new ArrayList<>();
+
+    public static ArrayList<String> retrieveFileNames(File[] fileArray) {
+        String line;
+        for(int i = 0; i < fileArray.length; i++) {
+            Date date = new Date(fileArray[i].lastModified());
+            if(fileArray[i].isDirectory()){
+                line = fileArray[i] + " [D]" + " - " + date;
+                fileList.add(line);
+                File[] subArray = fileArray[i].listFiles();
+                retrieveFileNames(subArray);
             } else {
-                writeToFile(element + "(F)");
+                line = fileArray[i] + " [F]" + " - " + date;
+                fileList.add(line);
             }
+        }
+        return fileList;
+    }
+
+    public static String[] stringArrayFromPath(String path){
+        return path.split("\\\\");
+    }
+
+    public static void createTree(ArrayList<String> arrayList) {
+        String branch;
+        String tab = "---";
+        for(String element : arrayList){
+            String[] pathArray = stringArrayFromPath(element);
+            int arrayLength = pathArray.length;
+            branch = tab.repeat(arrayLength - 1) + pathArray[arrayLength - 1];
+            writeToFile(branch, new File("directoryList.txt"));
         }
     }
 
-    public static void writeToFile(String str) {
-        try (FileWriter fileWriter = new FileWriter("directoryList.txt", true);
+    public static void writeToFile(String str, File file) {
+        try (FileWriter fileWriter = new FileWriter(file, true);
              BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
             bufferedWriter.write(str + "\n");
         } catch (IOException e) {
@@ -27,7 +51,7 @@ public class FileHandler {
         }
     }
 
-    public static void readTxt(File file) {
+    public static void readTxtFile(File file) {
         try(Scanner fileInput = new Scanner(file)) {
             while(fileInput.hasNextLine()){
                 System.out.println(fileInput.nextLine());
