baf70da33d87622b1ae77735eaeaff26fbbc5c99
Ignasi <imoyaf@gmail.com>
2024-05-28 10:08:30 +0200

    Update Main.java

--- src/sprint1/tasca5/n1exercici3/Main.java
+++ src/sprint1/tasca5/n1exercici3/Main.java
@@ -1,11 +1,13 @@
 package sprint1.tasca5.n1exercici3;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class Main {
+
     public static void main(String[] args) {
         Directory dir = new Directory("..");
         File[] fileArray = dir.listContentsOfDir(dir.getDirPathName());
-        FileHandler.loopThroughContent(fileArray);
+        FileHandler.createTree(FileHandler.retrieveFileNames(fileArray));
     }
 }
