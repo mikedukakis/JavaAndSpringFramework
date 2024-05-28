e12b1522a0fca99d19ab5cc6a7bde6f839fb4497
Ignasi Moya <imoyaf+git@gmail.com>
2024-05-28 13:44:51 +0200

    First commit

--- src/sprint1/tasca7/n1exerici2/Main.java
+++ src/sprint1/tasca7/n1exerici2/Main.java
@@ -2,11 +2,12 @@ package sprint1.tasca7.n1exerici2;
 
 public class Main {
 
+    @SuppressWarnings("deprecation")
+
     public static void main(String[] args) {
         OfficeEmployee.setFuel((float)125);
         OfficeEmployee emp1 = new OfficeEmployee("Jane", "Doe", (float)12.5);
         RemoteEmployee emp2 = new RemoteEmployee("John", "Doe", (float)12.5);
-        Employee emp3 = new Employee("Joan", "Petit", (float)12);
 
         emp1.obsoleteMethod();
         emp2.obsoleteMethod();
