5615df823a7324ba5afd700315b9d6f604f0a4a9
Ignasi Moya <imoyaf@gmail.com>
2024-07-22 12:29:15 +0200

    Edits

--- src/sprint1/tasca1/n1exercici1/InstrumentsVent.java
+++ src/sprint1/tasca1/n1exercici1/InstrumentsVent.java
@@ -1,7 +1,6 @@
 package sprint1.tasca1.n1exercici1;
 
 public class InstrumentsVent extends InstrumentsMusicals{
-    // Constructor
     public InstrumentsVent(String nom, double preu) {
         super(nom, preu);
         System.out.println("Classe carregada des del constructor de la subclasse: " + this.getClass());
@@ -11,7 +10,6 @@ public class InstrumentsVent extends InstrumentsMusicals{
         System.out.println("Classe carregada des d'un bloc d'inicialització estàtic de la subclasse: " + InstrumentsVent.class);
     }
 
-    // Mètodes
     @Override
     public void tocar() {
         System.out.println("Està sonant un instrument de vent");
